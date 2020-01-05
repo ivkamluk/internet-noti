d70dc8a9ee602d738a30c51837881eb4477c7d19
Kamluk Ivan <kamluk.jeck3ser@gmail.com>
2020-01-05 23:29:21 +0200

    Notification for Windows was added

--- desktop - Linux, Mac, Windows/notification.py
+++ desktop - Linux, Mac, Windows/notification.py
@@ -1,4 +1,5 @@
 import noti_constants
+import time
 from logger import logger
 import platform
 
@@ -12,11 +13,17 @@ def show_notification(website_status, website_url):
                                     noti_constants.ICON_NOTI_TRAY
                                    )
         noti.show()
-    elif platform.system() == 'Darwin': # MacOS
+    elif platform.system() == 'Darwin': #MacOS
         import pync
         pync.notify(title='internet-noti ', message=website_url,
                     subtitle=website_status, remove ='ALL')
-
+    elif platform.system() == 'Windows':
+        from plyer import notification
+        notification.notify(title='internet-noti ',
+                            message= website_status + website_url,
+                            app_icon = None,
+                            timeout = 10,
+                            )
     # -----
     logger.debug('Notification displayed for system > %s', platform.system())
     # -----
